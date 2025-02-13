fc6397a91e3b63563102a4b6e8f516e3d69f691c
Kieran Borg <kierans.borg@gmail.com>
2025-02-13 18:14:52 +0100

    warp finished

--- DS3Tool/services/BonfireService.cs
+++ DS3Tool/services/BonfireService.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using static DS3Tool.MainWindow;
 
 namespace DS3Tool.services
@@ -20,69 +18,24 @@ namespace DS3Tool.services
         private const int GameManOff = 0x4743AB0;
         private const int SprjLuaEventManOff = 0x473A9C8;
         private readonly DS3Process _process;
-        public Dictionary<string, BonfireLocation> BonfireLocations;
-        public BonfireService(DS3Process _ds3Process) {
+        public Dictionary<string, BonfireLocation> _bonfireDict;
+        public BonfireService(DS3Process _ds3Process, Dictionary<string, BonfireLocation> bonfireDict) {
 
             _process = _ds3Process;
-            readBonfiresFromCSV();
+            _bonfireDict = bonfireDict;
             codeCaveOffset = _process.ds3Base + 0x270BC20;
         }
 
-       
-
-        public struct BonfireLocation
-        {
-            public int Offset { get; }
-            public int StartBit { get; }
-            public int Id { get; }
-
-            public BonfireLocation(int offset, int startBit, int Id)
-            {
-                Offset = offset;
-                StartBit = startBit;
-                this.Id = Id;
-            }
-        }
-
-
-        private void readBonfiresFromCSV()
-        {
-            BonfireLocations = new Dictionary<string, BonfireLocation>();
-
-            string projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName;
-            string path = Path.Combine(projectDirectory, "data", "bonfires.csv");
-            string[] lines = File.ReadAllLines(path);
-
-            for (int i = 1; i < lines.Length; i++)
-            {
-                string[] columns = lines[i].Split(',');
-                if (columns.Length >= 4)
-                {
-                    string name = columns[0].Trim('"', ' ');
-                    int offset = Convert.ToInt32(columns[1].Trim('"', ' '), 16);
-
-                    int startBit = int.Parse(columns[2].Trim('"', ' '));
-                    int id = int.Parse(columns[3].Trim('"', ' '));
-
-                    BonfireLocation bonfireLocation = new BonfireLocation(offset, startBit, id);
-
-                    BonfireLocations[name] = bonfireLocation;
-                }
-            }
-        }
-
-
         public void warpToBonfire(string selectedBonfire)
         {
-            InitCodeCave(BonfireLocations[selectedBonfire].Id);
-            _process.ReadTestFull(codeBlockStart);
+            InitCodeCave(_bonfireDict[selectedBonfire].Id);
+
             _process.RunThread(codeBlockStart);
             Thread.Sleep(100);
-            CleanUpCodeCave();
+           CleanUpCodeCave();
 
         }
 
-       
         private void InitCodeCave(int id)
         {
             
@@ -99,6 +52,7 @@ namespace DS3Tool.services
 
             int eventOffset = (int)(_process.ds3Base.ToInt64() + SprjLuaEventManOff - (codeBlockStart.ToInt64() + codeCave.Length + 4));
 
+
             codeCave = codeCave.Concat(BitConverter.GetBytes(eventOffset))
                 .Concat(new byte[]
                 {
@@ -109,11 +63,8 @@ namespace DS3Tool.services
             
             codeCave = codeCave.Concat(BitConverter.GetBytes(idRelativeOffset))
                 .Concat(new byte[]
-                {
-                    0x45, 0x8D, 0x80, 0x18, 0xFC, 0xFF, 0xFF,
-                    0x45, 0x31, 0xC9,
+                {                  
                     0x48, 0x8B, 0x15,
-
                 }).ToArray();
 
             int bonfireStateOffset = (int) (_process.ds3Base.ToInt64() + GameManOff + 0xACC - (codeBlockStart.ToInt64() + codeCave.Length + 4));
@@ -131,14 +82,10 @@ namespace DS3Tool.services
                 .Concat(new byte[]
                 {
                     0x48, 0x83, 0xC4, 0x48,
-
                     0xC3
-
-
                 }).ToArray();
 
             _process.WriteBytes(codeBlockStart, codeCave);
-
             
         }
 
@@ -149,17 +96,14 @@ namespace DS3Tool.services
 
         }
 
-
-
-
         public void unlockAllBonfires()
         {
-            foreach (var bonfire in BonfireLocations.Keys)
+            foreach (var bonfire in _bonfireDict.Keys)
             {
                 var ptr1 = _process.ReadUInt64(_process.ds3Base + GameFlagDataOff);
                 var ptr2 = _process.ReadUInt64((IntPtr)ptr1);
 
-                var flagLoc = BonfireLocations[bonfire];
+                var flagLoc = _bonfireDict[bonfire];
 
                 var finalAddress = (IntPtr)(ptr2 + (ulong)flagLoc.Offset);
 
